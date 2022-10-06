be0952ef47fda8a456ea9d3a8d4d5e53cffd50de
Romka <101732278+Romchico@users.noreply.github.com>
2022-10-06 13:05:01 +0300

    Update Project_solution.sql

--- Project_solution.sql
+++ Project_solution.sql
@@ -40,6 +40,6 @@ WHERE station_name = 'DADAR' AND destination_station_name = 'CST-MUMBAI'
 AND departure_time IN ('09:00:00','10:00:00','11:00:00')
 ORDER BY departure_time ASC
 
-mdi
+![image](https://user-images.githubusercontent.com/101732278/194285723-6ebb996c-5779-4752-b977-7d899d0c9405.png)
 
 
