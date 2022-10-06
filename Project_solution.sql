7f891fafcf06278bfbff26c57381dc715c3983db
Romka <101732278+Romchico@users.noreply.github.com>
2022-10-06 14:12:45 +0300

    Update Project_solution.sql

--- Project_solution.sql
+++ Project_solution.sql
@@ -27,17 +27,6 @@ ALTER TABLE traindetails RENAME COLUMN arrival_time TO x
 ALTER TABLE traindetails RENAME COLUMN departure_time TO arrival_time
 ALTER TABLE traindetails RENAME COLUMN x TO departure_time
 
-/* Now I want to extract top 10 the most frequent train services: */
-SELECT station_name,destination_station_name,COUNT(station_name) FROM traindetails 
-GROUP BY station_name,destination_station_name
-ORDER BY COUNT(station_name) DESC LIMIT 10
-
-
-
-/* I choose to travel from DADAR to CST-MUMBAI. I choose the departure time between 9 to 11 AM. */
-SELECT departure_time,arrival_time FROM traindetails
-WHERE station_name = 'DADAR' AND destination_station_name = 'CST-MUMBAI'
-AND departure_time IN ('09:00:00','10:00:00','11:00:00')
-ORDER BY departure_time ASC
+SELECT * FROM traindetails LIMIT 5
 
 
